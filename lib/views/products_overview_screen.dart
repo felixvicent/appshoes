ed48322837006e057a8ea551170f2055933224f0
Felix Vicente <felixvicent1306@gmail.com>
2022-01-06 13:25:45 -0300

    Filter by favorites

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,16 +1,54 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, import_of_legacy_library_into_null_safe
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, import_of_legacy_library_into_null_safe, constant_identifier_names, unused_local_variable
 
 import 'package:appshoes/widgets/product_grid.dart';
 import 'package:flutter/material.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOption {
+  Favorite,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  State<ProductsOverviewScreen> createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  bool _showFavoriteOnly = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOption selectedValue) {
+              if (selectedValue == FilterOption.Favorite) {
+                setState(() {
+                  _showFavoriteOnly = true;
+                });
+              } else {
+                setState(() {
+                  _showFavoriteOnly = false;
+                });
+              }
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente favoritos'),
+                value: FilterOption.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOption.All,
+              )
+            ],
+          )
+        ],
       ),
-      body: ProductGrid(),
+      body: ProductGrid(_showFavoriteOnly),
     );
   }
 }
--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_constructors_in_immutables
 
 import 'package:appshoes/providers/products.dart';
 import 'package:appshoes/widgets/product_item.dart';
@@ -6,10 +6,16 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class ProductGrid extends StatelessWidget {
+  final bool showFavoriteOnly;
+
+  ProductGrid(this.showFavoriteOnly);
+
   @override
   Widget build(BuildContext context) {
     final productsProvider = Provider.of<Products>(context);
-    final products = productsProvider.items;
+    final products = showFavoriteOnly
+        ? productsProvider.favoriteItems
+        : productsProvider.items;
 
     return GridView.builder(
       padding: const EdgeInsets.all(10),
